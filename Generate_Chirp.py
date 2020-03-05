cc28cf2936f2febc39f6987a446f0ae4ba274104
jjriveramena <56571223+jjriveramena@users.noreply.github.com>
2020-03-04 20:08:41 -0800

    Update Generate_Chirp.py

--- Generate_Chirp.py
+++ Generate_Chirp.py
@@ -4,12 +4,12 @@ from scipy.signal import chirp, spectrogram
 import matplotlib.pyplot as plt
 import numpy as np
 
-num_secs = 10
+num_secs = .05
 
 t = np.linspace(0, num_secs, 5001)
-w = chirp(t, f0=4000, f1=6000, t1=num_secs, method='linear' )
+w = chirp(t, f0=1, f1=6000, t1=num_secs, method='logarithmic' )
 
-plt.plot(t[:500],w[:500])
-plt.title("Linear Chirp, f(0) = 1, f(10) = 10")
+plt.plot(t,w)
+plt.title("Linear Chirp, f(0) = 1, f(10) = 6kHz")
 plt.xlabel('Seconds (s)')
 plt.show()
